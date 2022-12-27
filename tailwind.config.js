167eb7d1d8663a4dac1080e2ce02bf31b7711bea
ha4219 <ha4219@naver.com>
2022-12-27 22:15:23 +0900

    font setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: ["./src/pages/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
+      ...defaultTheme.screens,
       xs: "320px",
     },
     extend: {},
