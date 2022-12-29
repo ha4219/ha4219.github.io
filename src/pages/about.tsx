e179ab1ca9a5d45baa1bd1d100b41da7c8456f7b
ha4219 <ha4219@naver.com>
2022-12-29 22:44:32 +0900

    add footer

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React from "react";
 
@@ -6,6 +7,7 @@ const AboutPage = () => {
     <>
       <Header />
       about
+      <Footer />
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <>
       <Header />
       <h1>Hello world!</h1>
-      <div className="font-bold text-3xl">Hello world!</div>
+      <div className="text-3xl font-bold">Hello world!</div>
+      <Footer />
     </>
   );
 };
