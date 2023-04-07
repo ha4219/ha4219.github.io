13a95e2952776dde185e6106b910714ba6d810a4
ha4219 <ha4219@naver.com>
2023-04-07 21:39:18 +0900

    edit about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,17 @@ import { PageProps } from "gatsby";
 import React from "react";
 
 const AboutPage: React.FC<PageProps> = () => {
-  return <Layout>about</Layout>;
+  return (
+    <Layout>
+      <div>
+        <span>hello i'm jeongdongha</span>
+      </div>
+      <div>
+        <span>github: </span>
+        <a href="https://github.com/ha4219">@ha4219</a>
+      </div>
+    </Layout>
+  );
 };
 
 export default AboutPage;
